0a0b66cdf791edc1ca5ee74ed1eb9e24a68f7750
joy <wendokingoo@gmail.com>
2019-03-16 22:01:24 +0300

    Models and views updated

--- api/models.py
+++ api/models.py
@@ -5,5 +5,18 @@ import datetime as dt
 class Project(models.Model):
     name = models.CharField(max_length=60)
     image = models.ImageField(upload_to="image/")
-    Details = models.TextField()
+    details = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def display_images(cls):
+        return cls.objects.all()
+
+    @classmethod
+    def index(cls):
+        today = dt.date.today()
+        yo = cls.objects.filter(pub_date__date=today)
+        return yo
--- api/views.py
+++ api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 import datetime as dt
+from .models import Project
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
+    images = Project.display_images()
     date = dt.date.today()   
   
-    return render(request, 'photos/index.html', {"date": date,})
+    return render(request, 'photos/index.html', {"images":images,"date": date,})
