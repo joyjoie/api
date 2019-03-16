87b95b4472cbead4710b8f443c6869825e539b21
joy <wendokingoo@gmail.com>
2019-03-16 23:51:36 +0300

    Profile page modified

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    
+    url(r'^profile/', views.profile, name='profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 import datetime as dt
-from .models import Project
+from .models import Project,Profile
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
@@ -10,3 +10,10 @@ def index(request):
     date = dt.date.today()   
   
     return render(request, 'photos/index.html', {"images":images,"date": date,})
+
+@login_required
+def profile(request):
+    fo=Profile.pro()
+    # profile=Profile.objects.get(id=id)
+    # current_profile=Profile.objects.get(user=request.user)
+    return render(request, 'profile/profile.html', {"fo":fo,"profile":profile})
