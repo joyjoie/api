43aad24adbc4b202a2e2029c014848289b24480f
joy <wendokingoo@gmail.com>
2019-03-16 22:11:41 +0300

    Profile class added in models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import datetime as dt
 
 # Create your models here.
@@ -20,3 +21,33 @@ class Project(models.Model):
         today = dt.date.today()
         yo = cls.objects.filter(pub_date__date=today)
         return yo
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=60 ,blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+       self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_bio(self, bio):
+        self.bio = bio
+        self.save()
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = Profile.objects.filter(user__username__icontains = name)
+        return profile
+
+    @classmethod
+    def pro(cls):
+        return cls.objects.all()
