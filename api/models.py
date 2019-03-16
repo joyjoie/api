39346c0c1c657c345e35dd2bfb9d8d6bac38ba50
joy <wendokingoo@gmail.com>
2019-03-16 13:06:57 +0300

    Navbar modified

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=60)
+    image = models.ImageField(upload_to="image/")
+    Details = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
