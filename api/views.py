94d384752dbf041a37400d567f1a9fbccf640647
joy <wendokingoo@gmail.com>
2019-03-17 01:43:45 +0300

    Update Profile function added and  modified

--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@ from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 import datetime as dt
 from .models import Project,Profile
+from . forms import ProfileUpdateForm
+from django.contrib import messages
+
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
@@ -14,6 +17,18 @@ def index(request):
 @login_required
 def profile(request):
     fo=Profile.pro()
-    # profile=Profile.objects.get(id=id)
-    # current_profile=Profile.objects.get(user=request.user)
-    return render(request, 'profile/profile.html', {"fo":fo,"profile":profile})
+    if request.method == 'POST':
+       
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if  p_form.is_valid():
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    return render(request, 'profile/profile.html', {"fo":fo,"profile":profile,"p_form":p_form})
