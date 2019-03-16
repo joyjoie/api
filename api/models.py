31147504363d9555f8370d2e5dd8bf94e61b6359
joy <wendokingoo@gmail.com>
2019-03-17 00:28:37 +0300

    Profile pclass  modified

--- api/models.py
+++ api/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Project(models.Model):
@@ -51,3 +54,13 @@ class Profile(models.Model):
     @classmethod
     def pro(cls):
         return cls.objects.all()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
