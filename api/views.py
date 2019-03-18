c0d2ca0566d309a2ec95e1c1372f2d27d4feba62
joy <wendokingoo@gmail.com>
2019-03-18 14:57:26 +0300

    Image views updated

--- api/views.py
+++ api/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 import datetime as dt
-from .models import Project,Profile,Comments
-from . forms import ProfileUpdateForm,CommentForm,ProjectForm
+from .models import Project,Profile,Comments,Ratings
+from . forms import ProfileUpdateForm,CommentForm,ProjectForm,RatingsForm
 from django.contrib import messages
 
 # Create your views here.
@@ -26,36 +26,17 @@ def index(request):
         form=CommentForm()
 
 
-    if request.method=='POST':
-        form=RatingsForm(request.POST)
-        if form.is_valid():
-            image_id=int(request.POST.get('image_id'))
-            image=Project.objects.get(id=image_id)
-            rating=form.save(commit=False)
-            rating.img=image
-            rating.user=request.user
-            rating.save()
-            return redirect('index')
-    else:
-        form=RatingsForm()
+   
 
-    context ={"images":images, "comments":comments , "form":form}
+    context ={"images":images, "comments":comments , "form":form, }
         
     return render(request, 'photos/index.html',context )
 
-def image(request, id,slug):
-    image=get_object_or_404()
-    try: 
-        foto = Image.objects.get(id = image_id, slug=slug)
+def image(request, id,):
+    imge=Project.objects.get(id=id)
+    current_project=Project.objects.get(user=request.user)
 
-    except DoesNotExist:
-        raise Http404()
-
-    # is_liked = False
-    # if image.likes.filter(id = request.user.id).exists():
-    #     is_liked = True
-
-    return render(request,"photos/image.html", {"foto":foto,"image":image})
+    return render(request,"photos/image.html", {"foto":foto,"imge":imge,"current_project":current_project})
 
 @login_required(login_url='/accounts/login/')
 def my_profile(request):
@@ -100,7 +81,7 @@ def profile(request,id):
     profile=Profile.objects.get(id=id)
     current_profile=Profile.objects.get(user=request.user)
 
-    return render(request, 'profile/profile.html', {"fo":fo,"profile":profile,"p_form":p_form,"profile":profile, "current_profile":current_profile})
+    return render(request, 'profile/profile.html', {"fo":fo,"p_form":p_form,"profile":profile, "current_profile":current_profile})
 
 
 def upload(request):
@@ -113,4 +94,19 @@ def upload(request):
         form=ProjectForm()
     return render(request, 'photos/addimg.html', {"form":form})
 
+def rating(request):
 
+    if request.method=='POST':
+        form=RatingsForm(request.POST)
+        if form.is_valid():
+            image_id=int(request.POST.get('image_id'))
+            image=Project.objects.get(id=image_id)
+            rating=form.save(commit=False)
+            rating.img=image
+            rating.user=request.user
+            rating.save()
+            return redirect('index')
+    else:
+        form=RatingsForm()
+
+    return render(request, 'photos/addimgratings.html', {"form":RatingsForm})
